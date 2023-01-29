6bfbeada212ba04398aec7b79e1175ad2d79db3c
Nezamov.Andrei <lynch.0945@gmail.com>
2023-01-29 11:07:47 +0900

    added homework04

---
+++ homework04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while (current <= Number)
+{
+    if (current % 2 == 0)
+    {
+        Console.WriteLine(current);
+        current++;
+    }
+    else
+    {
+        current++;
+    }
+}
