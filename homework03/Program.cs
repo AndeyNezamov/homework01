9cfc64a09ba76db429e6ec49a9d72ffb424b713f
Nezamov.Andrei <lynch.0945@gmail.com>
2023-01-29 11:05:54 +0900

    added homework03

---
+++ homework03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Давайте узнаем, является ли ваше число четным");
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
