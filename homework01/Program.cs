aa9ed9df9cdb3a8775b67fff702039127d65050c
Nezamov.Andrei <lynch.0945@gmail.com>
2023-01-29 11:00:19 +0900

    added homework01

---
+++ homework01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+if(NumberA>NumberB)
+{
+    Console.WriteLine("Max= " + NumberA + " Min= " + NumberB);
+}
+else
+{
+    Console.WriteLine("Max= " + NumberB + " Min=" + NumberA);
+}
